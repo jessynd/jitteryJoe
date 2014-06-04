ed8eada7503a87ccc05ca2f9e65a58da582a31f4
Michael Atlin <mike.atlin@gmail.com>
2014-06-04 12:01:41 -0400

    Renamed 'Title' 'In one word'

--- template.php
+++ template.php
@@ -9,12 +9,11 @@ function webcrafttheme_form_alter(&$form, &$form_state, $form_id) {
 
   if ($form_id == "user_feedback_node_form") {
     //add wrapper around title
-    $form["field_rating"]["und"]["#title"] = "Yo, I'm a title";
+    $form["title"]["#title"] = "In one word";
     $form["field_rating"]["und"]["#required"] = false;
- 	$form["field_rating"]["und"]["#prefix"] = "<div class='abc col-sm-12'>";
- 	$form["field_rating"]["und"]["#suffix"] = "</div>";
+ 	  $form["field_rating"]["und"]["#prefix"] = "<div class='abc col-sm-12'>";
+ 	  $form["field_rating"]["und"]["#suffix"] = "</div>";
      //add wrapper around ratings radio buttons
   }
-
 }
 
